3eb584633a3b3fdc186885014ab99be676b84aed
warmke <piotr.warmke@gmail.com>
2022-05-12 03:13:18 +0200

    Added buttons handling for menu

--- src/interface/Button.hpp
+++ src/interface/Button.hpp
@@ -4,11 +4,13 @@
 #include <string>
 
 namespace SimpleSnake::interface {
-class Button : public sf::Drawable {
+class Button : public sf::Drawable, public sf::Transformable {
 public:
+  Button() = default;
   Button(const std::string &label);
   void draw(sf::RenderTarget &target,
             const sf::RenderStates &states) const override;
+  void setActive(bool active);
 
 private:
   sf::Sprite m_buttonSprite;
