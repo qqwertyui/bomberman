e1b0671fba2b0182f74de5e8f7b168f29168448f
warmke <piotr.warmke@gmail.com>
2025-03-26 03:54:41 +0000

    Add basic client/server communication interface

--- src/client/GlobalConfig.hpp
+++ src/client/GlobalConfig.hpp
@@ -1,105 +1,15 @@
 #pragma once
 
-#include <cstdint>
-#include <string>
-#include <type_traits>
-#include <vector>
+#include "common/GlobalConfigBase.hpp"
 
 namespace bomberman {
-class ConfigLoader;
-
-// integral types
-template <typename T, std::enable_if_t<std::is_integral<T>::value &&
-                                           not std::is_same<T, bool>::value,
-                                       bool> = true>
-inline T fromString(const std::string &value) {
-  return std::stoi(value);
-}
-template <typename T, std::enable_if_t<std::is_integral<T>::value &&
-                                           not std::is_same<T, bool>::value,
-                                       bool> = true>
-inline std::string toString(const T &value) {
-  return std::to_string(value);
-}
-
-// std::string
-template <typename T,
-          std::enable_if_t<std::is_same<T, std::string>::value, bool> = true>
-inline std::string fromString(const std::string &value) {
-  return value;
-}
-template <typename T,
-          std::enable_if_t<std::is_same<T, std::string>::value, bool> = true>
-inline std::string toString(const T &value) {
-  return value;
-}
-
-// boolean types
-template <typename T,
-          std::enable_if_t<std::is_same<T, bool>::value, bool> = true>
-inline bool fromString(const std::string &value) {
-  return (value.compare("true") == 0);
-}
-template <typename T,
-          std::enable_if_t<std::is_same<T, bool>::value, bool> = true>
-inline std::string toString(const T &value) {
-  return value ? "true" : "false";
-}
-
-class ParameterBase {
+class GlobalConfig : public GlobalConfigBase {
 public:
-  virtual std::string asString() const = 0;
-  virtual std::string getName() const = 0;
-  virtual void loadFromString(const std::string &newValue) = 0;
-};
-
-#define PARAMETER(type, name, value)                                           \
-  class class_##name : public ParameterBase {                                  \
-  public:                                                                      \
-    class_##name() { GlobalConfig::parameters.push_back(this); }               \
-    std::string asString() const override {                                    \
-      return #name + std::string("=") + toString<type>(m_value);               \
-    }                                                                          \
-    std::string getName() const override { return #name; }                     \
-    type &operator()() { return m_value; }                                     \
-    void loadFromString(const std::string &newValue) override {                \
-      m_value = fromString<type>(newValue);                                    \
-    }                                                                          \
-                                                                               \
-  private:                                                                     \
-    type m_value{value};                                                       \
-  } name;
-
-class GlobalConfig {
-public:
-  GlobalConfig(const GlobalConfig &) = delete;
-  GlobalConfig(const GlobalConfig &&) = delete;
-  GlobalConfig &operator=(const GlobalConfig &) = delete;
-  GlobalConfig &operator=(const GlobalConfig &&) = delete;
+  using GlobalConfigBase::GlobalConfigBase;
 
   inline static GlobalConfig &get() {
     static GlobalConfig globalConfig;
     return globalConfig;
-  }
-
-  inline std::string asString() {
-    std::string result;
-    for (auto &parameter : parameters) {
-      result += parameter->asString() + ",";
-    }
-    result.pop_back();
-    return result;
-  }
-  inline std::string getRegisteredNames() {
-    std::string result;
-    for (auto &parameter : parameters) {
-      result += parameter->getName() + ",";
-    }
-    result.pop_back();
-    return result;
-  }
-  inline const std::vector<ParameterBase *> &getRegisteredParameters() {
-    return parameters;
   }
 
   PARAMETER(bool, isHelp, false);
@@ -112,9 +22,6 @@ public:
 
 private:
   GlobalConfig() = default;
-
-  static inline std::vector<ParameterBase *> parameters;
-  friend class ConfigLoader;
 };
 
 } // namespace bomberman
