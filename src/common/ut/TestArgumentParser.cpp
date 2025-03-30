3aca0481600e459b1f69fb93a7ccb3242244b91f
warmke <piotr.warmke@gmail.com>
2025-03-30 00:59:45 +0000

    Change CLI argument parsing format
    This commit changes the way CLI arguments are passed by
    removing replacing an equals '=' sign with whitespace ' '
    
    An example:
    ./bomberman-server --port=1111
    |
    v
    ./bomberman-server --port 1111

--- src/common/ut/TestArgumentParser.cpp
+++ src/common/ut/TestArgumentParser.cpp
@@ -8,55 +8,73 @@ protected:
   ArgumentParser sut;
 };
 
+TEST_F(TestArgumentParser, testIfEmptyInput) {
+  std::vector<std::string> args{};
+  auto result = sut.parse(args);
+
+  EXPECT_EQ(result.size(), 0);
+}
+
+TEST_F(TestArgumentParser, testIfNameOnlyArgument) {
+  std::vector<std::string> args{"--test"};
+  auto result = sut.parse(args);
+
+  EXPECT_EQ(result.size(), 1);
+  EXPECT_TRUE(result["test"] == "");
+}
+
+TEST_F(TestArgumentParser, testIfInvalidArgumentPrefix) {
+  std::vector<std::string> args{"-test", "asd"};
+  auto result = sut.parse(args);
+
+  EXPECT_EQ(result.size(), 0);
+}
+
 TEST_F(TestArgumentParser, testIfNumberOfOutputElementsIsOk1) {
-  std::vector<std::string> args{"--test=asd", "-c"};
+  std::vector<std::string> args{"--test", "asd"};
+  auto result = sut.parse(args);
+
+  EXPECT_EQ(result.size(), 1);
+  EXPECT_TRUE(result["test"] == "asd");
+}
+
+TEST_F(TestArgumentParser, testIfNameAndNameValueArgumentsAreTogether) {
+  std::vector<std::string> args{"--dummy", "--test", "asd"};
+  auto result = sut.parse(args);
+
+  EXPECT_EQ(result.size(), 2);
+  EXPECT_TRUE(result["dummy"] == "");
+  EXPECT_TRUE(result["test"] == "asd");
+}
+
+TEST_F(TestArgumentParser, testIfNumberOfOutputElementsIsOk2) {
+  std::vector<std::string> args{"--test", "asd", "--path", "asd"};
   auto result = sut.parse(args);
 
   EXPECT_EQ(result.size(), 2);
 }
 
-TEST_F(TestArgumentParser, testIfNumberOfOutputElementsIsOk2) {
-  std::vector<std::string> args{"--test=asd", "c", "-d", "123"};
-  auto result = sut.parse(args);
-
-  EXPECT_EQ(result.size(), 4);
-}
-
 TEST_F(TestArgumentParser, testIfCorrectlyMapped) {
-  std::vector<std::string> args{"--test=asd", "c",       "-d",
-                                "123",        "-c-=aaa", "-d---=--fsd"};
+  std::vector<std::string> args{"--test", "asd", "--path", "/home/admin/test"};
   auto result = sut.parse(args);
 
   EXPECT_TRUE(result["test"] == "asd");
-  EXPECT_TRUE(result["c"] == "");
-  EXPECT_TRUE(result["d"] == "");
-  EXPECT_TRUE(result["123"] == "");
-  EXPECT_TRUE(result["c-"] == "aaa");
-  EXPECT_TRUE(result["d---"] == "--fsd");
+  EXPECT_TRUE(result["path"] == "/home/admin/test");
 }
 
 TEST_F(TestArgumentParser, testIfNonExistentArgument) {
-  std::vector<std::string> args{"--test=asd"};
+  std::vector<std::string> args{"--test", "asd"};
   auto result = sut.parse(args);
 
   EXPECT_TRUE(result["test"] == "asd");
+  int i = 0;
   try {
-    result.at("--test");
-    EXPECT_TRUE(false);
+    result.at("path");
+    i = 1;
   } catch (const std::out_of_range &e) {
+    i = 2;
   }
-  try {
-    result.at("-test");
-    EXPECT_TRUE(false);
-  } catch (const std::out_of_range &e) {
-  }
-}
-
-TEST_F(TestArgumentParser, testIfContainsEqualsSign) {
-  std::vector<std::string> args{"--test==asd"};
-  auto result = sut.parse(args);
-
-  EXPECT_TRUE(result["test"] == "=asd");
+  EXPECT_EQ(i, 2);
 }
 
 int main(int argc, char **argv) {
