91573d49c34ee03197970504840bc4eb5640e5de
warmke <piotr.warmke@gmail.com>
2025-04-01 03:01:01 +0000

    Add basic game interface on client side
    -Create movable object (Circle) and means to move it around
    the map with keyboard (arrows up/down/right/left)
    -Send current position to server and listen for acknowledgment

--- src/client/scene/game/Scene.hpp
+++ src/client/scene/game/Scene.hpp
@@ -1,15 +1,23 @@
 #pragma once
 
 #include "scene/SceneBase.hpp"
+#include <memory>
 
 namespace bm::scene::game {
 class Scene : public SceneBase {
 public:
   Scene(SceneManager &sceneMgr);
 
+  void onEntry() override;
+  void onLeave() override;
+
   void handleEvents() override;
   void update() override;
   void draw() override;
+
+private:
+  struct impl;
+  std::unique_ptr<impl> pimpl;
 };
 
 } // namespace bm::scene::game
