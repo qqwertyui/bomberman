557acfcf3c2cd315f427280734b0fb7c58b5d52f
warmke <piotr.warmke@gmail.com>
2025-04-07 00:31:30 +0000

    Add Player & MessageHandler classes to store related logic
    -Player - class responsible for player logic, including movement,
    texture management and all other related stuff
    
    -MessageHandler - implements doubly buffered message queue. Active
    buffer is used by producer thread (threadMainLoop method) while passive
    is read directly from main thread which handles game loop (consumer).

---
+++ src/client/scene/game/MessageHandler.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "common/ConnectionManager.hpp"
+#include "common/itf/core.pb.h"
+#include <array>
+#include <string>
+#include <thread>
+
+namespace bm::scene::game {
+class MessageHandler {
+public:
+  MessageHandler(const std::string &ip, unsigned int port);
+  ~MessageHandler();
+
+  bool isConnected() const;
+  void handle();
+  bool send(common::itf::C2SMessage &req);
+
+private:
+  static void threadMainLoop(MessageHandler *self);
+
+  static constexpr unsigned int queueSize{100};
+  struct Buffer {
+    std::array<common::itf::S2CMessage, queueSize> elements;
+    int size{};
+  };
+
+  Buffer &getActiveBuffer();
+  Buffer &getPassiveBuffer();
+  void swapBuffers();
+  int getNextIndex(int index) const;
+  bool push(Buffer &buffer, common::itf::S2CMessage &element);
+
+  static constexpr unsigned int numberOfBuffers{2};
+  std::array<Buffer, numberOfBuffers> buffers;
+  unsigned int activeBufferIndex, passiveBufferIndex;
+
+  common::ConnectionManager connMgr;
+  std::thread serverMessageHandler;
+};
+
+} // namespace bm::scene::game
