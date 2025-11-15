f1a35c1deacbb0292922911233582eccfb08f6db
warmke <piotr.warmke@gmail.com>
2025-11-15 05:05:32 +0000

    Extract Widget management logic pt.2
    Remove redundant code and use WidgetManager where needed

--- src/client/scene/lobby/Scene.hpp
+++ src/client/scene/lobby/Scene.hpp
@@ -1,8 +1,7 @@
 #pragma once
 
-#include "gui/Button.hpp"
+#include "gui/WidgetManager.hpp"
 #include "scene/SceneBase.hpp"
-#include <map>
 #include <vector>
 
 namespace bm::scene::lobby {
@@ -14,25 +13,16 @@ public:
   void update() override;
   void draw() override;
   void onEntry() override;
-  void onLeave() override;
 
 private:
   using LobbyData = std::vector<std::pair<int, int>>;
-  using ButtonMap = std::map<int, gui::Button>;
 
   void createLobbyButton(const LobbyData &lobbyData);
-  void handleMouseClick(const sf::Mouse::Button &button);
   void handleKeyEvent(const sf::Keyboard::Scancode &scancode);
-  std::optional<unsigned int>
-  getSelectedButtonId(const sf::Vector2f &mousePos) const;
   bool joinLobby(unsigned int lobbyId);
   LobbyData fetchLobbyInfo();
 
   LobbyData lobbyData;
-  ButtonMap lobbyButtons;
-
-  bool keyboardActive{true};
-  int activeLobbyButton{0};
-  const int buttonInColumn{4};
+  gui::WidgetManager widgetManager;
 };
 } // namespace bm::scene::lobby
